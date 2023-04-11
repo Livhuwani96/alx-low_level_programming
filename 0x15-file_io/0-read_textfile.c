9e094fbb2704ac51bfb3d107c799ffaeef90d5e6
Livhuwani <livhuwani96@gmail.com>
2023-04-11 11:38:21 -0700

    task 0.1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,19 +8,20 @@
  * Return: w- actual number of bytes read and printed
  *        0 when function fails or filename is NULL.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
 	ssize_t fd;
 	ssize_t w;
-	ssize_t text;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	text = read(fd, buf, letters);
-	write = w(STDOUT_FILENO, buf, text);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
 	close(fd);
