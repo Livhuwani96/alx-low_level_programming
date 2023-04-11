88b0a0e49626812a7f3a1f2fa997543a3f3221c5
Livhuwani <livhuwani96@gmail.com>
2023-04-11 11:59:51 -0700

    task 01

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int filedata, write, lenght = 0;
+	int fd, w1, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (lenght = 0; text_content[lenght];)
-			lenght++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	filedata = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w1 = write(fd, text_content, len);
 
-	if (filedata == -1 || write == -1)
+	if (fd == -1 || w1 == -1)
 		return (-1);
 
-	close(filedata);
+	close(fd);
 
 	return (1);
 }
