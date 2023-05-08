7b56c34c2035d42c6a37b20c002b784a1f2fb158
livhuwani96 <livhuwani96@gmail.com>
2023-05-08 11:55:49 -0700

    0-read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,20 +11,21 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *u;
-	ssize_t d;
-	ssize_t m;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
 	ssize_t t;
 
-	fed = open(filename, O_RDONLY);
-	if (d == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	u = malloc(sizeof(char) * letters);
-	t = read(d, u, letters);
-	m = write(STDOUT_FILENO, u, t);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	free(u);
-	close(d);
-	return (t);
+	free(buf);
+	close(fd);
+	return (w);
 }
+
 
